acac2b44cf1cf87a8ae6d5fc9fce673ac5a38532
Manuel Rivera <marp.developer@gmail.com>
2023-04-23 19:48:22 -0500

    update dialog as sub-entry point

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { DialogModule } from '@mukhuve/ngx/dialog';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [importProvidersFrom(DialogModule)],
 });
--- projects/ngx/src/public-api.ts
+++ projects/ngx/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx
  */
 
-export * from './lib/dialog';
 // export * from './lib/stepper';
 export * from './lib/utils';
 // export * from './lib/toast';
